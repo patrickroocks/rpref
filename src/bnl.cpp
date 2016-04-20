fa7e35cca4d28dd553c522b14f2d3e225de91432
Patrick Roocks <patrick@ipep.de>
2016-04-20 08:51:27 +0200

    rPref 1.0.1
    fixed bug in top-k selection

--- src/bnl.cpp
+++ src/bnl.cpp
@@ -17,13 +17,13 @@ bool topk_setting::do_break(int level, int ntuples) { // gets level of this iter
 	if (and_connected) {
 		// Take intersection, break if one limit is reached
 		if ((topk     != -1 && ntuples >= topk)     || 
-			(at_least != -1 && ntuples >= at_least) || 
-			                   level   == toplevel) return(true);
+        (at_least != -1 && ntuples >= at_least) || 
+                           level   == toplevel) return(true);
 	} else {
 		// Take union, break if all limits are reached (or limits are not set)
 		if ((topk     == -1 || ntuples >= topk)      && 
-			(at_least == -1 || ntuples >= at_least)  && 
-			(toplevel == -1 || level   == toplevel)) return(true);
+        (at_least == -1 || ntuples >= at_least)  && 
+        (toplevel == -1 || level   >= toplevel)) return(true);
 	}
 	return(false);
 }
